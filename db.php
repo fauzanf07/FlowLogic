8465ed78932c32df89799d562d397ecb57f980a0
Fauzan Fiqriansyah <fauzanfiqriansyah126@gmail.com>
2023-08-02 22:43:29 +0700

    Changing connection

--- db.php
+++ db.php
@@ -1,3 +1,13 @@
 <?php
+$whitelist = array(
+    '127.0.0.1',
+    '::1'
+);
+
+if(in_array($_SERVER['REMOTE_ADDR'], $whitelist)){
     $con = mysqli_connect('localhost', 'root', '','db_gamifikasi');
+}else{
+    $con = mysqli_connect('localhost', 'stuz3844_db_gamifikasi', 'uzan7821','stuz3844_db_gamifikasi');
+}
+    
 ?>
